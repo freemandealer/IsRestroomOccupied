2fc1f3fdd3b48644a8687279392aaf8f95d259b0
freemandealer <freeman.zhang1992@gmail.com>
2022-07-01 00:09:14 +0800

    powersaving
    ref: https://github.com/esp8266/Arduino/issues/6642
    
    Signed-off-by: freemandealer <freeman.zhang1992@gmail.com>

--- wc_occupy_wifi.ino
+++ wc_occupy_wifi.ino
@@ -1,12 +1,23 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
+#include "user_interface.h"
+#include "gpio.h"
 
 #ifndef STASSID
 #define STASSID "CU_b659"
 #define STAPSK  "txrtv47h"
 #endif
+
+#define ENABLE_DEBUG 0
+void log(const char* str)
+{
+#if ENABLE_DEBUG
+  log(str);
+#else
+  return;
+#endif
+}
 
 ADC_MODE(ADC_VCC); // config A0 to read system voltage
 
@@ -16,19 +27,22 @@ const char* password = STAPSK;
 ESP8266WebServer server(80);
 
 const int led = LED_BUILTIN;
-const int OFF_THRESHOLD = 20;
+#if ENABLE_DEBUG
+const int OFF_THRESHOLD = 10;
+#else
+const int OFF_THRESHOLD = 60;
+#endif
 
 int inPin  = 5;
 int stat = LOW;
 unsigned short off_count = 0;
-bool wifi_on = 1;
 
 void handleRoot() {
   if (stat == HIGH) {
-    Serial.println("HIGH");
+    log("HIGH");
     server.send(200, "text/html", "<body style=\"background-color:#BA221F;\"/><h1 style=\"color:#FFFFFFFF\"/><center/>Occupied");
   } else {
-    Serial.println("LOW");
+    log("LOW");
     server.send(200, "text/html", "<body style=\"background-color:#77BA94;\"/><h1 style=\"color:#FFFFFFFF\"/><center/>Free");
   }      
 }
@@ -48,57 +62,87 @@ void handleNotFound() {
   server.send(404, "text/plain; charset=utf-8", message);
 }
 
+void wifi_start()
+{
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  if (WiFi.waitForConnectResult(20000) == WL_CONNECTED) {   // timeout 30s
+    log("");
+    log("Connected to ");
+    log(ssid);
+#if ENABLE_DEBUG
+    log("IP address: ");
+    Serial.println(WiFi.localIP());
+    uint8_t macAddr[6];
+    WiFi.macAddress(macAddr);
+    log("MAC address: ");
+    Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
+#endif
+  }
+  else {
+    log("connect timeout");
+    log("ignore this connection, pass to later sleep and wait for wakeup & retry connect");
+  }
+}
+
+void wifi_stop()
+{
+  WiFi.mode(WIFI_OFF);
+}
+
+void powersaving()
+{
+  log("powersaving");
+  wifi_fpm_set_sleep_type(LIGHT_SLEEP_T);
+  gpio_pin_wakeup_enable(GPIO_ID_PIN(inPin), GPIO_PIN_INTR_HILEVEL);
+  wifi_fpm_open();
+  wifi_fpm_do_sleep(0xFFFFFFF);
+  delay(10); // must delay, otherwise WDT will reset
+  /* ============================== */
+  log("wake up now");
+  /* ============================== */
+  wifi_fpm_do_wakeup();
+  wifi_fpm_close();
+  gpio_pin_wakeup_disable();
+}
+
 void setup(void) {
+  gpio_init();
   pinMode(led, OUTPUT);
   pinMode(inPin, INPUT);
   digitalWrite(led, HIGH); // HIGN means off
   Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("");
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  wifi_start();
 
-  if (MDNS.begin("esp8266")) {
-    Serial.println("MDNS responder started");
-  }
+  wifi_set_sleep_type(LIGHT_SLEEP_T);
+  log("light sleep mode enabled");
 
   server.on("/", handleRoot);
   server.onNotFound(handleNotFound);
 
   server.begin();
-  Serial.println("HTTP server started");
+  log("HTTP server started");
 }
 
 void loop(void) {
   stat = digitalRead(inPin);
   if (stat == HIGH ) {
     off_count = 0;
-    if (wifi_on == 0) {
-      Serial.print("Wake up wifi");
-      WiFi.mode( WIFI_STA);
-      WiFi.begin(ssid, password);
-      wifi_on = 1;
-    }
   }
   else {
     ++off_count;
-    if (off_count > OFF_THRESHOLD && wifi_on == 1) {
-      Serial.print("Turn off wifi to save energy");
-      WiFi.mode( WIFI_OFF);
-      wifi_on = 0;
+    if (off_count > OFF_THRESHOLD) {
+      log("go to sleep");
+      wifi_stop();
+      delay(10); // must delay, otherwise WDT will reset
+      powersaving();
+      wifi_start();
     }
   }
   server.handleClient();
-  MDNS.update();
+  digitalWrite(led, HIGH); // HIGN means off
   delay(1000);
 }
